48551d741176adbae69f253c8707d197e5156280
Prashanthcoder <tp120113@gmail.com>
2026-03-16 23:07:40 +0530

    added the javascript

---
+++ Javascript/meow.js
@@ -0,0 +1,26 @@
+document.write("Oh son cringe!")
+document.writeln("Mister chigma")
+document.writeln("Aren't you")
+document.write("<br>"
+)
+document.writeln("yes you are the one who is doing it")
+
+let str = "meow"
+str = "meoe"
+document.write("<br> ", str)
+
+//alert button
+// alert("Fill the images man first") //only okay button
+
+// //confirm statement
+// confirm("are you 18+")
+
+//prompt
+
+let myName = prompt("Enter the name")
+console.log(myName);
+let firstValue = Number(prompt("enter the value"))
+let secondValue = Number(prompt("enter the second value"))
+
+document.write(`<div id="jsdiv1" style="height:100px;width:300px;background:black;color:white;transform:scale(2);box-shadow:5px 5px 5px 5px gray;position:relative;top:200px;left:600px;display:flex;justify-content:center;align-items:center;text-shadow: 2px 2px 5px white">${myName}</div>`)
+document.write(`<div style="height: 100px;width:300px;background:darkblue;color:white;transform:scale(1.4);box-shadow: 0px 0px 5px 5px gray;position:relative;top:500px;left:600px;display:flex;justify-content:center;align-items:center;text-shadow:5px 5px 5px white">${firstValue + secondValue}</div>`);
