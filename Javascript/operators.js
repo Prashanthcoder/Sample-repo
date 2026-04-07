8f434862609d361ec4a5d78af110a7f2889173b9
Prashanthcoder <tp120113@gmail.com>
2026-04-07 23:59:29 +0530

    added the arrays methods

---
+++ Javascript/operators.js
@@ -0,0 +1,23 @@
+let arr = ['banana', 'apple', 'orange'];
+console.log(arr.slice(0, 2))
+console.log(arr)
+
+
+console.log(arr.splice(0, 2, 'grape')) 
+console.log(arr)
+
+arr.push('walnut')
+console.log(arr)
+
+arr.pop();
+console.log(arr)
+
+arr.shift()
+console.log(arr)
+
+arr.unshift('deleted fruit')
+console.log(arr)
+arr.push('apple')
+console.log(arr)
+arr.sort()
+console.log(arr)
